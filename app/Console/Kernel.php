760eb7c81397ac09f74e711d3c3b05555adcd885
Blessing <ajalablessing49@gmail.com>
2019-04-01 18:54:08 +0100

    initializing repository

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('fetch:inbox')->everyMinute();
+        
     }
 
     /**
